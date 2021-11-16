b485f313d89480b96f12abd683e545dda6d71893
Jan Nox <jan.nox@pm.me>
2021-11-16 21:27:07 +0100

    Add some minimal logging

--- SlaveProcess.py
+++ SlaveProcess.py
@@ -19,8 +19,10 @@ class SlaveProcess:
 
     # I will solely start the thread that executes the subprocess.
     def start(self):
+        noxLogger.info("SlaveProcess start for " + self.slaveItem.slave_name + " begins")
         self.slaveThread = threading.Thread(target=self.runThread)
         self.slaveThread.start()
+        noxLogger.info("SlaveProcess start for " + self.slaveItem.slave_name + " ends")
 
     # I will wait for the specified amount of seconds before anything will happen.
     def wait(self):
@@ -28,15 +30,19 @@ class SlaveProcess:
 
     # I will solely stop the slave process.
     def stop(self):
+        noxLogger.info("SlaveProcess stop for " + self.slaveItem.slave_name + " begins")
         self.slaveThread.raise_exception()
         self.slaveThread.join()
+        noxLogger.info("SlaveProcess stop for " + self.slaveItem.slave_name + " ends")
 
     # I will set the subProcess to run if not already done.
     def isRunning(self):
         if self.subProcess is None:
+            noxLogger.info("isRunning for " + self.slaveItem.slave_name + " will run the command now")
             self.subProcess = True
             subprocess.run(["python3.7", self.slaveItem.slave_directory + self.slaveItem.slave_command])
-            return True
+            noxLogger.error("isRunning for " + self.slaveItem.slave_name + " process stopped")
+            return False
 
     # I am the threads function that will run the process.
     def runThread(self):
